2142f3830828a1461b653c1f7c068e52c0376c82
Jakub Filak <jfilak@redhat.com>
2012-11-12 09:44:45 +0100

    Added support for native methods and unknown sources

--- lib/java_frame.c
+++ lib/java_frame.c
@@ -23,6 +23,9 @@
 #include "utils.h"
 #include <string.h>
 
+#define BTP_JF_MARK_NATIVE_METHOD "Native Method"
+#define BTP_JF_MARK_UNKNOWN_SOURCE "Unknown Source"
+
 struct btp_java_frame *
 btp_java_frame_new()
 {
@@ -95,6 +98,9 @@ btp_java_frame_cmp(struct btp_java_frame *frame1,
     if (res != 0)
         return res;
 
+    if (frame1->is_native != frame2->is_native)
+        return frame1->is_native ? 1 : -1;
+
     return frame1->file_line - frame2->file_line;
 }
 
@@ -103,13 +109,38 @@ btp_java_frame_append_to_str(struct btp_java_frame *frame,
                              struct btp_strbuf *dest)
 {
 
-    btp_strbuf_append_strf(dest, "\tat %s(%s",
-                           frame->function_name ? frame->function_name : "",
-                           frame->file_name ? frame->file_name : "");
+    btp_strbuf_append_strf(dest, "\tat %s(",
+                           frame->function_name ? frame->function_name : "");
+
+    if (frame->is_native)
+        btp_strbuf_append_str(dest, BTP_JF_MARK_NATIVE_METHOD);
+    else if (!frame->file_name)
+        btp_strbuf_append_str(dest, BTP_JF_MARK_UNKNOWN_SOURCE);
+    else
+        btp_strbuf_append_str(dest, frame->file_name);
+
+    /* YES even if the frame is native method or source is unknown */
+    /* WHY? Because it was parsed in this form */
+    /* Ooops! Maybe the source file was empty string. Don't care! */
     if (frame->file_line)
         btp_strbuf_append_strf(dest, ":%d", frame->file_line);
 
     btp_strbuf_append_str(dest, ")");
+}
+
+/*
+ * We can expect different formats hence these two following helper functions
+ */
+static bool btp_java_frame_parse_is_native_method(const char *input_mark)
+{
+    return 0 == strncmp(input_mark, BTP_JF_MARK_NATIVE_METHOD,
+                                    strlen(BTP_JF_MARK_NATIVE_METHOD));
+}
+
+static bool btp_java_frame_parse_is_unknown_source(const char *input_mark)
+{
+    return 0 == strncmp(input_mark, BTP_JF_MARK_UNKNOWN_SOURCE,
+                                    strlen(BTP_JF_MARK_UNKNOWN_SOURCE));
 }
 
 struct btp_java_frame *
@@ -153,7 +184,13 @@ btp_java_frame_parse(const char **input,
         btp_location_add(location, 0, btp_skip_char_cspan(&cursor, ":)\n"));
 
         if (mark != cursor)
-            frame->file_name = btp_strndup(mark, cursor - mark);
+        {
+            if (btp_java_frame_parse_is_native_method(mark))
+                frame->is_native = true;
+            else if (!btp_java_frame_parse_is_unknown_source(mark))
+                /* DO NOT set file_name if input says that source isn't known */
+                frame->file_name = btp_strndup(mark, cursor - mark);
+        }
 
         if (*cursor == ':')
         {
