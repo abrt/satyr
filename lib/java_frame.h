a45612aea927b1e80c5501bc935958ad18f3945a
Richard Marko <rmarko@redhat.com>
2013-01-15 13:49:37 +0100

    Fix case in #define

--- lib/java_frame.h
+++ lib/java_frame.h
@@ -18,8 +18,8 @@
     with this program; if not, write to the Free Software Foundation, Inc.,
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
-#ifndef BTPARSER_java_FRAME_H
-#define BTPARSER_java_FRAME_H
+#ifndef BTPARSER_JAVA_FRAME_H
+#define BTPARSER_JAVA_FRAME_H
 
 /**
  * @file
