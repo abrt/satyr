abrt/satyr

commit 93d2ef8b730bc2659e348bce9f7ecfc902600b9d

python: get rid of explicit getters and setters
This commit does three slightly different things:

1. It replaces most of the explicit get_ and set_ methods in the Python
   API with (computed) attribute access [1], making it slightly less
   cumbersome to use.

2. In the python bindings, the getters and setters that had to be
   defined for each attribute as two functions have been replaced with
   generic versions that need to be written only once for each member
   type.

   The generic [gs]etters access the members using their structure
   offsets. Preprocessor macros are provided to compute the offsets.
   Such approach is generally somewhat unsafe as it bypasses the type
   system - as a compensation, test cases are present which check that
   access to all the attributes works correctly.

3. The SYMS_{OK,WRONG,NOT_FOUND} constants were moved from the
   GdbSharedlib class to the module level.

Related to #59 and #69.

[1] http://docs.python.org/2.7/c-api/typeobj.html#PyTypeObject.tp_getset

Signed-off-by: Martin Milata <mmilata@redhat.com>
Signed-off-by: Jakub Filak <jfilak@redhat.com>

python/py_common.c
@@ -1,0 +1,127 @@
+/*
+    py_common.c
+
+    Copyright (C) 2013 Red Hat, Inc.
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#include "py_common.h"
+
+#include "lib/utils.h"
+
+/*
+ * Generic getters/setters that accept a struct getset_offsets as the data
+ * argument.
+ */
+
+PyObject *
+sr_py_getter_string(PyObject *self, void *data)
+{
+    struct getset_offsets *gsoff = data;
+    char *str = MEMB_T(char*, MEMB(self, gsoff->c_struct_offset), gsoff->member_offset);
+
+    if (str == NULL)
+        Py_RETURN_NONE;
+
+    return PyString_FromString(str);
+}
+
+int
+sr_py_setter_string(PyObject *self, PyObject *rhs, void *data)
+{
+    struct getset_offsets *gsoff = data;
+
+    char *newvalue = PyString_AsString(rhs);
+    if (!newvalue)
+        return -1;
+
+    char *str = MEMB_T(char*, MEMB(self, gsoff->c_struct_offset), gsoff->member_offset);
+    free(str);
+    MEMB_T(char*, MEMB(self, gsoff->c_struct_offset), gsoff->member_offset) = sr_strdup(newvalue);
+
+    return 0;
+}
+
+PyObject *
+sr_py_getter_uint32(PyObject *self, void *data)
+{
+    struct getset_offsets *gsoff = data;
+    uint32_t num = MEMB_T(uint32_t, MEMB(self, gsoff->c_struct_offset), gsoff->member_offset);
+    return PyInt_FromLong(num);
+}
+
+int
+sr_py_setter_uint32(PyObject *self, PyObject *rhs, void *data)
+{
+    struct getset_offsets *gsoff = data;
+
+    long newvalue = PyInt_AsLong(rhs);
+    if (PyErr_Occurred())
+        return -1;
+
+    if (newvalue < 0)
+    {
+        PyErr_SetString(PyExc_ValueError, "Value must not be negative.");
+        return -1;
+    }
+
+    MEMB_T(uint32_t, MEMB(self, gsoff->c_struct_offset), gsoff->member_offset)
+        = (uint32_t)newvalue;
+    return 0;
+}
+
+PyObject *
+sr_py_getter_bool(PyObject *self, void *data)
+{
+    struct getset_offsets *gsoff = data;
+
+    bool b = MEMB_T(bool, MEMB(self, gsoff->c_struct_offset), gsoff->member_offset);
+    return PyBool_FromLong(b);
+}
+
+int
+sr_py_setter_bool(PyObject *self, PyObject *rhs, void *data)
+{
+    struct getset_offsets *gsoff = data;
+
+    bool newvalue = PyObject_IsTrue(rhs);
+    MEMB_T(bool, MEMB(self, gsoff->c_struct_offset), gsoff->member_offset) = newvalue;
+
+    return 0;
+}
+
+PyObject *
+sr_py_getter_uint64(PyObject *self, void *data)
+{
+    struct getset_offsets *gsoff = data;
+
+    uint64_t num
+        = MEMB_T(uint64_t, MEMB(self, gsoff->c_struct_offset), gsoff->member_offset);
+    return PyLong_FromUnsignedLongLong(num);
+}
+
+int
+sr_py_setter_uint64(PyObject *self, PyObject *rhs, void *data)
+{
+    struct getset_offsets *gsoff = data;
+
+    unsigned long long newvalue = PyInt_AsUnsignedLongLongMask(rhs);
+    if (PyErr_Occurred())
+        return -1;
+
+    MEMB_T(uint64_t, MEMB(self, gsoff->c_struct_offset), gsoff->member_offset) = newvalue;
+    return 0;
+}

python/py_gdb_sharedlib.c
@@ -1,61 +1,32 @@
+#include "py_common.h"
 #include "py_gdb_sharedlib.h"
 #include "lib/gdb_sharedlib.h"
 #include "lib/utils.h"
 
 #define sharedlib_doc "satyr.GdbSharedlib - class representing a shared library loaded at the moment of crash"
 
-#define s_get_from_doc "Usage: sharedlib.get_from()\n" \
-                       "Returns: long - address where lib's memory begins"
+#define s_symbols_doc "Symbol state (satyr.SYMS_OK / satyr.SYMS_NOT_FOUND / satyr.SYMS_WRONG)\n"         \
+                      "SYMS_OK:        Debug symbols for the library were loaded successfully.\n"        \
+                      "SYMS_WRONG:     Debug symbols for the library were present, but did not match.\n" \
+                      "SYMS_NOT_FOUND: Debug symbols for the library were not found."
 
-#define s_set_from_doc "Usage: sharedlib.set_from(newvalue)\n" \
-                       "Newvalue: long - address where lib's memory begins"
+/* See python/py_common.h and python/py_gdb_frame.c for generic getters/setters documentation. */
+#define GSOFF_PY_STRUCT sr_py_gdb_sharedlib
+#define GSOFF_PY_MEMBER sharedlib
+#define GSOFF_C_STRUCT sr_gdb_sharedlib
+GSOFF_START
+GSOFF_MEMBER(from),
+GSOFF_MEMBER(to),
+GSOFF_MEMBER(soname)
+GSOFF_END
 
-#define s_get_to_doc "Usage: sharedlib.get_to()\n" \
-                     "Returns: long - address where lib's memory ends"
-
-#define s_set_to_doc "Usage: sharedlib.set_to(newvalue)\n" \
-                     "Newvalue: long - address where lib's memory ends"
-
-#define s_get_symbols_doc "Usage: sharedlib.get_symbols()\n" \
-                          "Returns: SYMS_OK / SYMS_NOT_FOUND / SYMS_WRONG"
-
-#define s_set_symbols_doc "Usage: sharedlib.set_symbols(newvalue)\n" \
-                          "Newvalue: SYMS_OK / SYMS_NOT_FOUND / SYMS_WRONG"
-
-#define s_get_soname_doc "Usage: sharedlib.get_soname()\n" \
-                         "Returns: string - library filename"
-
-#define s_set_soname_doc "Usage: sharedlib.set_soname(newvalue)\n" \
-                         "Newvalue: string - library filename"
-
-#define s_syms_ok_doc (char *)"Constant. Debug symbols for the library were loaded successfully."
-
-#define s_syms_wrong_doc (char *)"Constant. Debug symbols for the library were present, but did not match."
-
-#define s_syms_not_found_doc (char *)"Constant. Debug symbols for the library were not found."
-
-static PyMethodDef
-gdb_sharedlib_methods[] =
+static PyGetSetDef
+gdb_sharedlib_getset[] =
 {
-    /* getters & setters */
-    { "get_from",    sr_py_gdb_sharedlib_get_from,    METH_NOARGS,  s_get_from_doc    },
-    { "set_from",    sr_py_gdb_sharedlib_set_from,    METH_VARARGS, s_set_from_doc    },
-    { "get_to",      sr_py_gdb_sharedlib_get_to,      METH_NOARGS,  s_get_to_doc      },
-    { "set_to",      sr_py_gdb_sharedlib_set_to,      METH_VARARGS, s_set_to_doc      },
-    { "get_symbols", sr_py_gdb_sharedlib_get_symbols, METH_NOARGS,  s_get_symbols_doc },
-    { "set_symbols", sr_py_gdb_sharedlib_set_symbols, METH_VARARGS, s_set_symbols_doc },
-    { "get_soname",  sr_py_gdb_sharedlib_get_soname,  METH_NOARGS,  s_get_soname_doc  },
-    { "set_soname",  sr_py_gdb_sharedlib_set_soname,  METH_VARARGS, s_set_soname_doc  },
-    /* methods */
-    { NULL },
-};
-
-static PyMemberDef
-gdb_sharedlib_members[] =
-{
-    { (char *)"SYMS_OK",        T_INT, offsetof(struct sr_py_gdb_sharedlib, syms_ok),        READONLY, s_syms_ok_doc        },
-    { (char *)"SYMS_WRONG",     T_INT, offsetof(struct sr_py_gdb_sharedlib, syms_wrong),     READONLY, s_syms_wrong_doc     },
-    { (char *)"SYMS_NOT_FOUND", T_INT, offsetof(struct sr_py_gdb_sharedlib, syms_not_found), READONLY, s_syms_not_found_doc },
+    SR_ATTRIBUTE_UINT64_R("start_address", from, "Address where lib's memory begins (long)"),
+    SR_ATTRIBUTE_UINT64_R("end_address",   to,   "Address where lib's memory ends (long)"  ),
+    SR_ATTRIBUTE_STRING(soname, "Library file name (string)"),
+    { (char*)"symbols", sr_py_gdb_sharedlib_get_symbols, sr_py_gdb_sharedlib_set_symbols, (char*)s_symbols_doc, NULL },
     { NULL },
 };
 
@@ -90,9 +61,9 @@ sr_py_gdb_sharedlib_type =
     0,                          /* tp_weaklistoffset */
     NULL,                       /* tp_iter */
     NULL,                       /* tp_iternext */
-    gdb_sharedlib_methods,      /* tp_methods */
-    gdb_sharedlib_members,      /* tp_members */
-    NULL,                       /* tp_getset */
+    NULL,                       /* tp_methods */
+    NULL,                       /* tp_members */
+    gdb_sharedlib_getset,       /* tp_getset */
     NULL,                       /* tp_base */
     NULL,                       /* tp_dict */
     NULL,                       /* tp_descr_get */
@@ -122,9 +93,6 @@ sr_py_gdb_sharedlib_new(PyTypeObject *object, PyObject *args, PyObject *kwds)
         return PyErr_NoMemory();
 
     so->sharedlib = sr_gdb_sharedlib_new();
-    so->syms_ok = SYMS_OK;
-    so->syms_wrong = SYMS_WRONG;
-    so->syms_not_found = SYMS_NOT_FOUND;
 
     return (PyObject *)so;
 }
@@ -147,80 +115,26 @@ sr_py_gdb_sharedlib_str(PyObject *self)
 
 /* getters & setters */
 
-/* function_name */
 PyObject *
-sr_py_gdb_sharedlib_get_from(PyObject *self, PyObject *args)
-{
-    return Py_BuildValue("l", ((struct sr_py_gdb_sharedlib*)self)->sharedlib->from);
-}
-
-PyObject *
-sr_py_gdb_sharedlib_set_from(PyObject *self, PyObject *args)
-{
-    unsigned long long newvalue;
-    if (!PyArg_ParseTuple(args, "l", &newvalue))
-        return NULL;
-
-    ((struct sr_py_gdb_sharedlib *)self)->sharedlib->from = newvalue;
-    Py_RETURN_NONE;
-}
-
-PyObject *
-sr_py_gdb_sharedlib_get_to(PyObject *self, PyObject *args)
-{
-    return Py_BuildValue("l", ((struct sr_py_gdb_sharedlib*)self)->sharedlib->to);
-}
-
-PyObject *
-sr_py_gdb_sharedlib_set_to(PyObject *self, PyObject *args)
-{
-    unsigned long long newvalue;
-    if (!PyArg_ParseTuple(args, "l", &newvalue))
-        return NULL;
-
-    ((struct sr_py_gdb_sharedlib *)self)->sharedlib->to = newvalue;
-    Py_RETURN_NONE;
-}
-
-PyObject *
-sr_py_gdb_sharedlib_get_symbols(PyObject *self, PyObject *args)
+sr_py_gdb_sharedlib_get_symbols(PyObject *self, void *data)
 {
     return Py_BuildValue("i", ((struct sr_py_gdb_sharedlib*)self)->sharedlib->symbols);
 }
 
-PyObject *
-sr_py_gdb_sharedlib_set_symbols(PyObject *self, PyObject *args)
+int
+sr_py_gdb_sharedlib_set_symbols(PyObject *self, PyObject *rhs, void *data)
 {
-    int newvalue;
-    if (!PyArg_ParseTuple(args, "i", &newvalue))
-        return NULL;
+    long newvalue = PyInt_AsLong(rhs);
+    if (PyErr_Occurred())
+        return -1;
 
     if (newvalue != SYMS_OK && newvalue != SYMS_WRONG && newvalue != SYMS_NOT_FOUND)
     {
         PyErr_SetString(PyExc_ValueError, "Symbols must be either SYMS_OK, "
                                           "SYMS_WRONG or SYMS_NOT_FOUND.");
-        return NULL;
+        return -1;
     }
 
     ((struct sr_py_gdb_sharedlib *)self)->sharedlib->symbols = newvalue;
-    Py_RETURN_NONE;
-}
-
-PyObject *
-sr_py_gdb_sharedlib_get_soname(PyObject *self, PyObject *args)
-{
-    return Py_BuildValue("s", ((struct sr_py_gdb_sharedlib*)self)->sharedlib->soname);
-}
-
-PyObject *
-sr_py_gdb_sharedlib_set_soname(PyObject *self, PyObject *args)
-{
-    char *soname;
-    if (!PyArg_ParseTuple(args, "s", &soname))
-        return NULL;
-
-    struct sr_py_gdb_sharedlib *this = (struct sr_py_gdb_sharedlib*)self;
-    free(this->sharedlib->soname);
-    this->sharedlib->soname = sr_strdup(soname);
-    Py_RETURN_NONE;
+    return 0;
 }

tests/python/koops.py
@@ -2,6 +2,7 @@
 
 import os
 import unittest
+from test_helpers import BindingsTestCase
 
 try:
     import _satyr as satyr
@@ -18,7 +19,7 @@ if not os.path.isfile(path):
 with file(path) as f:
     contents = f.read()
 
-class TestKerneloops(unittest.TestCase):
+class TestKerneloops(BindingsTestCase):
     def setUp(self):
         self.koops = satyr.Kerneloops(contents)
 
@@ -26,7 +27,10 @@ class TestKerneloops(unittest.TestCase):
         self.assertEqual(len(self.koops.frames), frames_expected)
 
     def test_correct_modules(self):
-        self.assertEqual(len(self.koops.get_modules()), mods_expected)
+        self.assertEqual(len(self.koops.modules), mods_expected)
+        ## not implemented, expected to fail
+        #self.koops.modules = ['foo', 'bar']
+        #self.assertEqual(len(self.koops.modules), 2)
 
     def test_dup(self):
         dup = self.koops.dup()
@@ -46,7 +50,7 @@ class TestKerneloops(unittest.TestCase):
         out = str(self.koops)
         self.assertTrue(('Kerneloops with %d frames' % frames_expected) in out)
 
-class TestKoopsFrame(unittest.TestCase):
+class TestKoopsFrame(BindingsTestCase):
     def setUp(self):
         self.frame = satyr.Kerneloops(contents).frames[0]
 
@@ -59,20 +63,33 @@ class TestKoopsFrame(unittest.TestCase):
 
     def test_dup(self):
         dup = self.frame.dup()
-        self.assertEqual(dup.get_function_name(),
-            self.frame.get_function_name())
+        self.assertEqual(dup.function_name,
+            self.frame.function_name)
 
-        dup.set_function_name('other')
-        self.assertNotEqual(dup.get_function_name(),
-            self.frame.get_function_name())
+        dup.function_name = 'other'
+        self.assertNotEqual(dup.function_name,
+            self.frame.function_name)
 
     def test_cmp(self):
         dup = self.frame.dup()
         self.assertEqual(dup.cmp(dup), 0)
         self.assertEqual(dup.cmp(self.frame), 0)
         self.assertEqual(dup.cmp(self.frame), 0)
-        dup.set_reliable(False)
+        dup.reliable = False
         self.assertNotEqual(dup.cmp(self.frame), 0)
+
+    def test_getset(self):
+        self.assertGetSetCorrect(self.frame, 'reliable', True, False)
+        self.assertGetSetCorrect(self.frame, 'address', 0xffffffff81057adfL, 60200)
+        self.assertGetSetCorrect(self.frame, 'function_name', 'warn_slowpath_common', 'fun_ction')
+        self.assertGetSetCorrect(self.frame, 'function_offset', 0x7f, 890)
+        self.assertGetSetCorrect(self.frame, 'function_length', 0xc0, 234)
+        self.assertGetSetCorrect(self.frame, 'module_name', None, 'dev_one.ko')
+        self.assertGetSetCorrect(self.frame, 'from_address', 0, 333333)
+        self.assertGetSetCorrect(self.frame, 'from_function_name', None, 'teh_caller')
+        self.assertGetSetCorrect(self.frame, 'from_function_offset', 0, 0x31337)
+        self.assertGetSetCorrect(self.frame, 'from_function_length', 0, 1024)
+        self.assertGetSetCorrect(self.frame, 'from_module_name', None, 'asdf')
 
 if __name__ == '__main__':
     unittest.main()

tests/python/test_helpers.py
@@ -1,0 +1,14 @@
+import unittest
+
+class BindingsTestCase(unittest.TestCase):
+    def assertGetSetCorrect(self, obj, attr, orig_val, new_val):
+        '''
+        Check whether getting/setting an attribute works correctly.
+        '''
+        self.assertEqual(obj.__getattribute__(attr), orig_val)
+        obj.__setattr__(attr, new_val)
+        self.assertEqual(obj.__getattribute__(attr), new_val)
+
+if __name__ == '__main__':
+    import sys
+    sys.exit('This module is not meant to be run directly')

python/Makefile.am
@@ -32,6 +32,8 @@ _satyr_la_SOURCES = \
     py_java_stacktrace.c \
     py_metrics.h \
     py_metrics.c \
+    py_common.h \
+    py_common.c \
     py_module.c
 
 _satyr_la_CPPFLAGS = \

python/py_common.h
@@ -1,0 +1,126 @@
+/*
+    py_common.h
+
+    Copyright (C) 2013 Red Hat, Inc.
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+#ifndef SATYR_PY_COMMON_H
+#define SATYR_PY_COMMON_H
+
+/**
+ * @file
+ * @brief Common routines for python bindings.
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <Python.h>
+#include <structmember.h>
+
+/*
+ * Struct member access using offsets. The second variant is useful for nested
+ * structures.
+ *
+ * MEMB_T(type, st, offsetof(typeof(st), member)) == (type)(st->member)
+ * MEMB(st, offsetof(typeof(st), member)) == (void*)(st->member)
+ */
+#define MEMB_T(type, st, off) *(type *)((char*)st + off)
+#define MEMB(st, off) MEMB_T(void*, st, off)
+
+/*
+ * This struct is used for generic getters and setters. Most of satyr's python
+ * object structures contain pointer to a corresponding C structure, which in
+ * turn contains the members that are exposed in the python API.
+ *
+ * If we know the offset of the pointer to the C structure in the python object
+ * and the offset of the member in the C structure, we can access the member
+ * using generic (per-type) functions, declared further below.
+ */
+struct getset_offsets
+{
+    size_t c_struct_offset;
+    size_t member_offset;
+};
+
+/*
+ * These macros make it possible to initialize several struct getset_offsets
+ * without typing too much and entering the offsets by hand.
+ *
+ * You need to define GSOFF_PY_STRUCT to be the type of the structure defining
+ * the python object, GSOFF_PY_MEMBER to be the name of its member pointing to
+ * the C structure, and GSOFF_C_STRUCT to be the type of the C structure. Use
+ * GSOFF_MEMBER for each member you want to access, GSOFF_MEMBER(m) defines the
+ * variable gsoff_m which can be used in the declaration of getters and
+ * setters.
+ *
+ * See python/py_gdb_frame.c for example usage.
+ */
+#define GSOFF_START static struct getset_offsets
+#define GSOFF_MEMBER(c_member)                                                \
+    gsoff_ ## c_member = {                                                    \
+        .c_struct_offset = offsetof(struct GSOFF_PY_STRUCT, GSOFF_PY_MEMBER), \
+        .member_offset = offsetof(struct GSOFF_C_STRUCT, c_member)            \
+    }
+#define GSOFF_END ;
+
+/*
+ * Once you declare GSOFF_MEMBER() macro for each member you want to expose in
+ * python, you can use following macros to define PyGetSetDef structures.
+ *
+ * The _R variants stand for "rename" in case you want the python attribute to
+ * have different than the corresponding C member.
+ *
+ * See python/py_gdb_sharedlib.c for sample usage of both.
+ */
+#define SR_ATTRIBUTE_STRING_R(name, c_member, doc_str) \
+        { (char*)name, sr_py_getter_string, sr_py_setter_string, (char*)doc_str, &gsoff_ ## c_member }
+#define SR_ATTRIBUTE_STRING(c_member, doc_str) SR_ATTRIBUTE_STRING_R(#c_member, c_member, doc_str)
+
+#define SR_ATTRIBUTE_UINT32_R(name, c_member, doc_str) \
+        { (char*)name, sr_py_getter_uint32, sr_py_setter_uint32, (char*)doc_str, &gsoff_ ## c_member }
+#define SR_ATTRIBUTE_UINT32(c_member, doc_str) SR_ATTRIBUTE_UINT32_R(#c_member, c_member, doc_str)
+
+#define SR_ATTRIBUTE_UINT64_R(name, c_member, doc_str) \
+        { (char*)name, sr_py_getter_uint64, sr_py_setter_uint64, (char*)doc_str, &gsoff_ ## c_member }
+#define SR_ATTRIBUTE_UINT64(c_member, doc_str) SR_ATTRIBUTE_UINT64_R(#c_member, c_member, doc_str)
+
+#define SR_ATTRIBUTE_BOOL_R(name, c_member, doc_str) \
+        { (char*)name, sr_py_getter_bool, sr_py_setter_bool, (char*)doc_str, &gsoff_ ## c_member }
+#define SR_ATTRIBUTE_BOOL(c_member, doc_str) SR_ATTRIBUTE_BOOL_R(#c_member, c_member, doc_str)
+
+/*
+ * Generic getters/setters that accept a struct getset_offsets as the data
+ * argument.
+ */
+PyObject *sr_py_getter_string(PyObject *self, void *data);
+int sr_py_setter_string(PyObject *self, PyObject *rhs, void *data);
+
+PyObject *sr_py_getter_uint32(PyObject *self, void *data);
+int sr_py_setter_uint32(PyObject *self, PyObject *rhs, void *data);
+
+PyObject *sr_py_getter_bool(PyObject *self, void *data);
+int sr_py_setter_bool(PyObject *self, PyObject *rhs, void *data);
+
+PyObject *sr_py_getter_uint64(PyObject *self, void *data);
+int sr_py_setter_uint64(PyObject *self, PyObject *rhs, void *data);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SATYR_PY_COMMON_H */

commit 9f2709fa6a61a367c93d13cdd8d23b6ac53f1407

python: fix attribute deletion segfault
Previous commit 93d2ef8b730bc2659e348bce9f7ecfc902600b9d introduced a
bug where an attempt to delete a computed attribute results in a
segmentation fault.

The setters in C bindings have been modified to check whether the new
parameter is NULL and throw an exception if it is.

Signed-off-by: Martin Milata <mmilata@redhat.com>
Signed-off-by: Richard Marko <rmarko@redhat.com>

python/py_common.c
@@ -42,6 +42,12 @@ sr_py_getter_string(PyObject *self, void *data)
 int
 sr_py_setter_string(PyObject *self, PyObject *rhs, void *data)
 {
+    if (rhs == NULL)
+    {
+        PyErr_SetString(PyExc_TypeError, "Cannot delete this attribute.");
+        return -1;
+    }
+
     struct getset_offsets *gsoff = data;
 
     char *newvalue = PyString_AsString(rhs);
@@ -66,6 +72,12 @@ sr_py_getter_uint32(PyObject *self, void *data)
 int
 sr_py_setter_uint32(PyObject *self, PyObject *rhs, void *data)
 {
+    if (rhs == NULL)
+    {
+        PyErr_SetString(PyExc_TypeError, "Cannot delete this attribute.");
+        return -1;
+    }
+
     struct getset_offsets *gsoff = data;
 
     long newvalue = PyInt_AsLong(rhs);
@@ -95,6 +107,12 @@ sr_py_getter_bool(PyObject *self, void *data)
 int
 sr_py_setter_bool(PyObject *self, PyObject *rhs, void *data)
 {
+    if (rhs == NULL)
+    {
+        PyErr_SetString(PyExc_TypeError, "Cannot delete this attribute.");
+        return -1;
+    }
+
     struct getset_offsets *gsoff = data;
 
     bool newvalue = PyObject_IsTrue(rhs);
@@ -116,6 +134,12 @@ sr_py_getter_uint64(PyObject *self, void *data)
 int
 sr_py_setter_uint64(PyObject *self, PyObject *rhs, void *data)
 {
+    if (rhs == NULL)
+    {
+        PyErr_SetString(PyExc_TypeError, "Cannot delete this attribute.");
+        return -1;
+    }
+
     struct getset_offsets *gsoff = data;
 
     unsigned long long newvalue = PyInt_AsUnsignedLongLongMask(rhs);

python/py_gdb_sharedlib.c
@@ -124,6 +124,12 @@ sr_py_gdb_sharedlib_get_symbols(PyObject *self, void *data)
 int
 sr_py_gdb_sharedlib_set_symbols(PyObject *self, PyObject *rhs, void *data)
 {
+    if (rhs == NULL)
+    {
+        PyErr_SetString(PyExc_TypeError, "Cannot delete this attribute.");
+        return -1;
+    }
+
     long newvalue = PyInt_AsLong(rhs);
     if (PyErr_Occurred())
         return -1;

tests/python/koops.py
@@ -31,6 +31,7 @@ class TestKerneloops(BindingsTestCase):
         ## not implemented, expected to fail
         #self.koops.modules = ['foo', 'bar']
         #self.assertEqual(len(self.koops.modules), 2)
+        self.assertRaises(NotImplementedError, self.koops.__delattr__, 'modules')
 
     def test_dup(self):
         dup = self.koops.dup()

tests/python/test_helpers.py
@@ -8,6 +8,7 @@ class BindingsTestCase(unittest.TestCase):
         self.assertEqual(obj.__getattribute__(attr), orig_val)
         obj.__setattr__(attr, new_val)
         self.assertEqual(obj.__getattribute__(attr), new_val)
+        self.assertRaises(TypeError, obj.__delattr__, attr)
 
 if __name__ == '__main__':
     import sys