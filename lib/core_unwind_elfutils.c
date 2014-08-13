29a96f46028808994dfaf03fb0c29dde9601f44e
Martin Milata <mmilata@redhat.com>
2014-08-13 17:48:04 +0200

    Revert usage of /proc/PID/maps
    Using maps breaks on backtraces with VDSO and the elfutils bug this may
    have worked around has been fixed:
    https://bugzilla.redhat.com/show_bug.cgi?id=1112610
    
    Fixes #174.
    
    Related elfutils RFE: https://bugzilla.redhat.com/show_bug.cgi?id=1129777
    
    Signed-off-by: Martin Milata <mmilata@redhat.com>

--- lib/core_unwind_elfutils.c
+++ lib/core_unwind_elfutils.c
@@ -126,10 +126,9 @@ abort:
 }
 
 struct sr_core_stacktrace *
-sr_parse_coredump_maps(const char *core_file,
-                       const char *exe_file,
-                       const char *maps_file,
-                       char **error_msg)
+sr_parse_coredump(const char *core_file,
+                  const char *exe_file,
+                  char **error_msg)
 {
     struct sr_core_stacktrace *stacktrace = NULL;
 
@@ -137,7 +136,7 @@ sr_parse_coredump_maps(const char *core_file,
     if (error_msg)
         *error_msg = NULL;
 
-    struct core_handle *ch = open_coredump(core_file, exe_file, maps_file, error_msg);
+    struct core_handle *ch = open_coredump(core_file, exe_file, error_msg);
     if (!ch)
         goto fail;
 
