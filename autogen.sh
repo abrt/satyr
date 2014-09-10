3bb367d3ef5e3c6f96e4e78c0325b283bbd7d275
Martin Milata <mmilata@redhat.com>
2014-09-10 12:04:02 +0200

    Include git commit in non-release version
    I don't like it very much but at least we'll be consistent with other
    ABRT projects.
    
    jfilak:
    I back ported this patch from master because of the same troubles with
    the ABRT integration tests as described in #184.
    
    Signed-off-by: Jakub Filak <jfilak@redhat.com>
    
    Conflicts:
    	autogen.sh
    	configure.ac

--- autogen.sh
+++ autogen.sh
@@ -1,1 +1,48 @@
-autoreconf --install --force
+#! /bin/sh
+
+print_help()
+{
+cat << EOH
+Prepares the source tree for configuration
+
+Usage:
+  autogen.sh [sydeps [--install]]
+
+Options:
+
+  sysdeps          prints out all dependencies
+    --install      install all dependencies ('sudo yum install \$DEPS')
+
+EOH
+}
+
+build_depslist()
+{
+    DEPS_LIST=`grep "^\(Build\)\?Requires:" *.spec.in | grep -v "%{name}" | tr -s " " | tr "," "\n" | cut -f2 -d " " | grep -v "^satyr" | sort -u | while read br; do if [ "%" = ${br:0:1} ]; then grep "%define $(echo $br | sed -e 's/%{\(.*\)}/\1/')" *.spec.in | tr -s " " | cut -f4 -d" "; else echo $br ;fi ; done | tr "\n" " "`
+}
+
+case "$1" in
+    "--help"|"-h")
+            print_help
+            exit 0
+        ;;
+    "sysdeps")
+            build_depslist
+
+            if [ "$2" == "--install" ]; then
+                set -x verbose
+                sudo yum install $DEPS_LIST
+                set +x verbose
+            else
+                echo $DEPS_LIST
+            fi
+            exit 0
+        ;;
+    *)
+            echo "Running gen-version"
+            ./gen-version
+
+            echo "Running autoreconf"
+            autoreconf --install --force
+        ;;
+esac
